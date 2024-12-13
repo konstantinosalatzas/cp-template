140a5a3bacea47047cf6e3c5477400d53aedaf5c
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-13 20:12:59 +0000

    Write Binary Search template code

--- algorithms/binary_search.cpp
+++ algorithms/binary_search.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
+int n, k, l, r, m;
+
 bool f(int i) {
-    return true;
+    return (i>=k);
 }
 
-void binary_search(int n) {
-    int l, r;
+void binary_search() {
     l=-1, r=n;
-    while (r-l>1) {
-        int m;
+    while(r-l>1) {
         m=l+(r-l)/2;
-        if (f(m)) {
+        if(f(m)) {
             r=m;
         }
         else {
@@ -24,5 +24,11 @@ void binary_search(int n) {
 int main()
 {
 	ios_base::sync_with_stdio(0), cin.tie(0);
+    cin>>n;
+    srand(time(NULL));
+    k=rand()%n;
+    cout<<k<<'\n';
+    binary_search();
+    cout<<l<<' '<<r<<'\n';
 	return 0;
 }
